e034df149390bfdf7ddce868d0299c8d73769e17
AyakaHashimoto <ayaka.seven.11@gmail.com>
2023-09-08 13:30:18 +0900

    編集エリアとラベルを追加

--- scripts.js
+++ scripts.js
@@ -7,69 +7,90 @@ document.getElementById('addTodo').addEventListener('click', function() {
     }
 });
 
-document.getElementById('todoList').addEventListener('click', function(event) {
-    if (event.target.tagName === 'LI' && event.target.nodeName !== 'BUTTON') {
-        const li = event.target;
-        const textNode = li.childNodes[0];
-        console.log("textNode,", textNode);
+document.getElementById('filterLabels').addEventListener('change', function() {
+    const selectedLabel = this.value;
 
-        const input = document.createElement('input');
-        const complatedBtn = document.createElement('button');    
-
-        input.type = 'text';
-        input.value = textNode.nodeValue;
-        li.appendChild(input);
-
-        input.focus();
-        input.select();
-
-        // Enterキーが押されたときのハンドラ
-        input.addEventListener('keydown', function(e) {
-            if (e.key === 'Enter') {
-                textNode.nodeValue = e.target.value;
-                complatedBtn.textContent = '完了';
-                complatedBtn.onclick = function() { // ボタンがクリックされたときの動作
-                    li.classList.toggle('completed');
-                    saveToLocalStorage();
-                };
-                input.removeEventListener('blur', handleBlur);
-                input.remove();
-                saveToLocalStorage(); 
-            }
-        });
-
-        // inputからフォーカスが外れたときのハンドラ
-        input.addEventListener('blur', handleBlur);
-
-        function handleBlur() {
-            textNode.nodeValue = input.value;
-            complatedBtn.textContent = '完了';
-            complatedBtn.onclick = function() { 
-                li.classList.toggle('completed');
-                saveToLocalStorage();
-            };
-            input.remove();
-            saveToLocalStorage();
-        }
-    }
+    filterTodosByLabel(selectedLabel);
 });
 
-function addToDo(text) {
+function filterTodosByLabel(label) {
+    const todos = document.querySelectorAll('#todoList li');
+
+    todos.forEach(todo => {
+        const todoLabel = todo.querySelector('.todo-label').textContent.trim();
+
+        if (label === "all" || todoLabel === label) {
+            todo.style.display = "";
+        } else {
+            todo.style.display = "none";
+        }
+    });
+}
+
+function addToDo(text, label = '') {
     const ul = document.getElementById('todoList');
     const li = document.createElement('li');
     const complatedBtn = document.createElement('button');
     const textNode = document.createTextNode(text);
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+    const labelSpan = document.createElement('span');
+    const labelText = document.createTextNode(label);
 
     complatedBtn.textContent = '完了';
     complatedBtn.onclick = function() {
         li.classList.toggle('completed');
         saveToLocalStorage();
     };
+    editBtn.textContent = '編集';
+    editBtn.onclick = function() {
+      showEditArea(li, textNode, labelSpan);
+    };
+    deleteBtn.textContent = '削除';
+    deleteBtn.onclick = function() {
+        ul.removeChild(li);
+        saveToLocalStorage();
+    }
+
+    labelSpan.className = 'todo-label';
+    if(!label.trim()) { // ラベルが空の場合
+        console.log('!label.trim()=== true')
+        labelSpan.classList.add('empty-label');
+    }
+    labelSpan.appendChild(labelText);
+    
     li.appendChild(textNode);
+    li.appendChild(labelSpan);
     ul.appendChild(li);
     li.appendChild(complatedBtn);
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
 }
 
+function showEditArea(li, textNode, labelSpan) {
+    document.getElementById('editArea').style.display = 'block';
+    document.getElementById('editText').value = textNode.nodeValue;
+    document.getElementById('editLabels').value = labelSpan.textContent;
+
+    document.getElementById('saveEdit').onclick = function() {
+        textNode.nodeValue = document.getElementById('editText').value;
+        labelSpan.textContent = document.getElementById('editLabels').value;
+
+        labelSpan.className = 'todo-label';
+        if(!labelSpan.textContent.trim()) { // ラベルが空の場合
+            console.log('!label.trim()=== true in showEditArea')
+            labelSpan.classList.add('empty-label');
+        }
+
+        document.getElementById('editArea').style.display = 'none';
+        saveToLocalStorage();
+    }
+    document.getElementById('cancelEdit').onclick = function() {
+        document.getElementById('editArea').style.display = 'none';
+    }   
+  }
+
+  
 
 function saveToLocalStorage() {
     const ul = document.getElementById('todoList');
@@ -77,8 +98,11 @@ function saveToLocalStorage() {
 
     for (let i = 0; i < ul.children.length; i++) {
         let taskText = ul.children[i].firstChild.nodeValue;
+        let taskLabel = ul.children[i].querySelector('.todo-label').textContent;
+
         todos.push({
             text: taskText,
+            label: taskLabel,
             completed: ul.children[i].classList.contains('completed')
         });
     }
@@ -91,7 +115,7 @@ function loadFromLocalStorage() {
 
     if (todos) {
         todos.forEach(function(todo) {
-            addToDo(todo.text);
+            addToDo(todo.text, todo.label);
             if (todo.completed) {
                 document.getElementById('todoList').lastChild.classList.add('completed');
             }
